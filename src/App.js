470d34e9243713b7c22b33fabfc7374ec6281248
Jonathon <cisneros.jonathon@gmail.com>
2022-04-29 17:58:06 -0700

    created RatingSelect and ability to add new feedback items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';  // unique ID package - Terminal: npm install uuid
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackForm from './components/FeedbackForm';
@@ -8,6 +9,12 @@ import FeedbackData from './data/FeedbackData';
 
 function App ( ) {
   const [ feedback, setFeedback ] = useState( FeedbackData );
+
+  const addFeedback = ( newFeedback ) => {
+    newFeedback.id = uuidv4( );
+    setFeedback( [ newFeedback, ...feedback ] );  // adds new feedback to list (on top)
+  }
+
   const deleteFeedback = ( id ) => {
     if (window.confirm('Are you sure?')) {
       setFeedback(feedback.filter( ( item ) => item.id !== id ))
@@ -18,7 +25,7 @@ function App ( ) {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={ addFeedback }/>
         <FeedbackStats feedback={ feedback }/>
         <FeedbackList feedback={ feedback } handleDelete= { deleteFeedback } />
       </div>
